98cc9c94a5429d3e7ef39955d46f36953d108313
alsrudrl1220 <alsrudrl1220@naver.com>
2023-10-13 16:53:46 +0900

    AccountAnalyzeService saveAmount logic modified to add transactional annotation

--- demo/src/main/java/com/example/demo/repository/PaymentRepository.java
+++ demo/src/main/java/com/example/demo/repository/PaymentRepository.java
@@ -4,11 +4,13 @@ import com.example.demo.entity.PaymentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository // 첫번째 인자 : 어떤 Entity인지, 두번째 인자 : pk 어떤 타입인지
 public interface PaymentRepository extends JpaRepository<PaymentEntity, String>
 {
     Optional<PaymentEntity> findByOrderId(String orderId);
+    List<PaymentEntity> findByMemberId(String memberId);
 }
 //PaymentRepository.interface
--- demo/src/main/java/com/example/demo/service/AccountAnalyzeService.java
+++ demo/src/main/java/com/example/demo/service/AccountAnalyzeService.java
@@ -11,6 +11,7 @@ import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,23 +19,34 @@ import java.util.Optional;
 
 @Service //스프링이 관리해주는 객체 == 스프링 빈
 @RequiredArgsConstructor //controller와 같이. final 멤버변수 생성자 만드는 역할
+@Transactional
 public class AccountAnalyzeService {
-
-    // private final MemberRepository memberRepository; // 먼저 jpa, mysql dependency 추가
-
     private final PaymentRepository paymentRepository;
     private final AccountAnalyzeRepository accountAnalyzeRepository;
 
     public MemberDTO saveAmount(MemberDTO memberDTO){ //entity객체는 service에서만
-        List<PaymentEntity> paymentAll = paymentRepository.findAll();
+        List<PaymentEntity> paymentAll = paymentRepository.findByMemberId(memberDTO.getMemberId()); // 해당 회원은 무조건 payment테이블에 결제내역이 1개 이상 있다고 가정 (나중에 예외처리 추가 해도 됨)
 
         for(PaymentEntity pay : paymentAll){
             Optional<AccountAnalyzeEntity> analyzeEntry = accountAnalyzeRepository.findByEntryAndMemberId(pay.getEntry(),pay.getMemberId());
             if(analyzeEntry.isPresent()){  // 분석 테이블에 이미 있는 항목이면 해당 항목에 값을 더해서 업데이트 하기
-                AccountAnalyzeEntity analyzeEntity = analyzeEntry.get();
-                AccountAnalyzeDTO dto = new AccountAnalyzeDTO(analyzeEntity.getAnalyzeId(), pay.getMemberId(), pay.getEntry(), pay.getAmount());
-                AccountAnalyzeEntity entity = AccountAnalyzeEntity.toAccountAnalyzeEntity(dto);
-                accountAnalyzeRepository.save(entity); // -> 원래 값이 update가 되는지, 새로 생성되는지 확인하기
+                // 1.
+                //AccountAnalyzeEntity analyzeEntity = analyzeEntry.get();
+                //AccountAnalyzeDTO dto = new AccountAnalyzeDTO(analyzeEntity.getAnalyzeId(), pay.getMemberId(), pay.getEntry(), pay.getAmount());
+                //AccountAnalyzeEntity entity = AccountAnalyzeEntity.toAccountAnalyzeEntity(dto);
+                //accountAnalyzeRepository.save(entity); // -> 원래 값이 update가 되는지, 새로 생성되는지 확인하기
+
+                //2.
+                //AccountAnalyzeEntity originEntity = analyzeEntry.get();
+                //Integer updateAmount = originEntity.getTotalAmount() + pay.getAmount();
+                //originEntity.setTotalAmount(updateAmount);
+                //accountAnalyzeRepository.save(originEntity);
+
+                //3.
+                AccountAnalyzeEntity originEntity = analyzeEntry.get();
+                Integer updateAmount = originEntity.getTotalAmount() + pay.getAmount();
+                originEntity.setTotalAmount(updateAmount);
+
             } else { // 분석 테이블에 없는 항목이면 새로 항목 추가 후 값 넣기
                 AccountAnalyzeDTO dto = new AccountAnalyzeDTO(pay.getMemberId(), pay.getEntry(), pay.getAmount());
                 AccountAnalyzeEntity entity = AccountAnalyzeEntity.toAccountAnalyzeEntity(dto);
