272be0b4e1cd8bf2046256f13dacf863b8c6be3e
alsrudrl1220 <alsrudrl1220@naver.com>
2023-11-14 17:24:37 +0900

    중간데모용 설문조사 무시 코드 구현

--- demo/src/main/java/com/example/demo/controller/SurveyController.java
+++ demo/src/main/java/com/example/demo/controller/SurveyController.java
@@ -27,38 +27,38 @@ public class SurveyController {
     // 설문조사 작성하고 db에 저장하기
     @PostMapping(value="/member/survey")
     public SurveyDTO setSurveyDTO(@RequestBody SurveyDTO surveyDTO){
-
-        System.out.println(surveyDTO);
-        surveyService.save(surveyDTO);
-
-        // 설문조사 끝나자 마자 최근 일주일 간의 결제내역 분석결과를 기반으로 미션 생성 (payment 객체 불러와서 AA table에 분석 결과가 저장되어있는 상태에서)
-
         // 미션, 피드백 문장을 만들기 위한 [ 해당 회원 id / 미션, 피드백 시작 날짜 정보 ]
         String selectedMemberId = surveyDTO.getSurveyId();
         String startDate = accountAnalyzeService.findThisWeek(selectedMemberId);
+        
+        if(surveyService.findBySurveyId(selectedMemberId) == null){ // 중간데모용 if문 (survey가 이미 있으면 설문조사 저장x, 미션 로직 x / 없으면 실행)
+            System.out.println(surveyDTO);
+            surveyService.save(surveyDTO);
+            
+            // 설문조사 끝나자 마자 최근 일주일 간의 결제내역 분석결과를 기반으로 미션 생성 (payment 객체 불러와서 AA table에 분석 결과가 저장되어있는 상태에서)
+            
+            // 1. 미션 로직을 통해 미션 항목 선정
+            missionService.mission(selectedMemberId);
 
-        // 1. 미션 로직을 통해 미션 항목 선정
-        missionService.mission(selectedMemberId);
+            // 2. 선정된 항목으로 미션 문장 만들고 저장
+            ChatGptResponse chatGptResponseForMission = null;
+            chatGptResponseForMission = chatGptService.askQuestionM(selectedMemberId, startDate);
+            String missionContent = chatGptResponseForMission.getChoices().get(0).getMessage().getContent();
+            System.out.println(missionContent);
+            missionService.saveMissionSen(selectedMemberId, startDate, missionContent);
 
-        // 2. 선정된 항목으로 미션 문장 만들고 저장
-        ChatGptResponse chatGptResponseForMission = null;
-        chatGptResponseForMission = chatGptService.askQuestionM(selectedMemberId, startDate);
-        String missionContent = chatGptResponseForMission.getChoices().get(0).getMessage().getContent();
-        System.out.println(missionContent);
-        missionService.saveMissionSen(selectedMemberId, startDate, missionContent);
+            // 3. 선정된 항목과 분석 테이블의 totalAmount를 토대로 피드백 문장 만들고 저장
+            ChatGptResponse chatGptResponseForFeedback = null;
+            chatGptResponseForFeedback = chatGptService.askQuestion(selectedMemberId, startDate);
+            String feedbackContent = chatGptResponseForFeedback.getChoices().get(0).getMessage().getContent();
+            feedbackService.save(selectedMemberId, feedbackContent, startDate);
 
-        // 3. 선정된 항목과 분석 테이블의 totalAmount를 토대로 피드백 문장 만들고 저장
-        ChatGptResponse chatGptResponseForFeedback = null;
-        chatGptResponseForFeedback = chatGptService.askQuestion(selectedMemberId, startDate);
-        String feedbackContent = chatGptResponseForFeedback.getChoices().get(0).getMessage().getContent();
-        feedbackService.save(selectedMemberId, feedbackContent, startDate);
+            // 4. 이번 주차 미션과 피드백 문장 생성 후 분석 테이블에 이번 주차 항목들의 OkToUse True로 변경
+            accountAnalyzeService.changeOkToUseWithTrue(selectedMemberId);
 
-        // 4. 이번 주차 미션과 피드백 문장 생성 후 분석 테이블에 이번 주차 항목들의 OkToUse True로 변경
-        accountAnalyzeService.changeOkToUseWithTrue(selectedMemberId);
-
-        // 5. profile goalEntry 업데이트
-        profileService.updateGoalEntry(selectedMemberId);
-
+            // 5. profile goalEntry 업데이트
+            profileService.updateGoalEntry(selectedMemberId);
+        }
         return surveyDTO;
     }
 
