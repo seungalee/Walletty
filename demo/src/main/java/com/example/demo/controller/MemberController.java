3f034c120f3b0ed858671c8698e548b18a7744dc
choiyukyung <yukyungchoi8@gmail.com>
2023-11-14 15:07:47 +0900

    surveyController 생성

--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -27,6 +27,7 @@ public class MemberController {
     private final MissionService missionService;
     private final FeedbackService feedbackService;
     private final ProfileService profileService;
+    private final SurveyService surveyService;
 
     // 회원가입 로직
     @PostMapping("/member/join")   // 나중에 RequestMapping으로 수정
@@ -111,74 +112,7 @@ public class MemberController {
         return loginResult;
     }
 
-    // ************* survey **************
 
-
-    private final SurveyService surveyService;
-
-    // 설문조사 작성하고 db에 저장하기
-    @PostMapping(value="/member/survey")
-    public SurveyDTO setSurveyDTO(@RequestBody SurveyDTO surveyDTO){
-
-        System.out.println(surveyDTO);
-        surveyService.save(surveyDTO);
-
-        // 설문조사 끝나자 마자 최근 일주일 간의 결제내역 분석결과를 기반으로 미션 생성 (payment 객체 불러와서 AA table에 분석 결과가 저장되어있는 상태에서)
-
-        // 미션, 피드백 문장을 만들기 위한 [ 해당 회원 id / 미션, 피드백 시작 날짜 정보 ]
-        String selectedMemberId = surveyDTO.getSurveyId();
-        String startDate = accountAnalyzeService.findThisWeek(selectedMemberId);
-
-        // 1. 미션 로직을 통해 미션 항목 선정
-        missionService.mission(selectedMemberId);
-
-        // 2. 선정된 항목으로 미션 문장 만들고 저장
-        ChatGptResponse chatGptResponseForMission = null;
-        chatGptResponseForMission = chatGptService.askQuestionM(selectedMemberId, startDate);
-        String missionContent = chatGptResponseForMission.getChoices().get(0).getMessage().getContent();
-        System.out.println(missionContent);
-        missionService.saveMissionSen(selectedMemberId, startDate, missionContent);
-
-        // 3. 선정된 항목과 분석 테이블의 totalAmount를 토대로 피드백 문장 만들고 저장
-        ChatGptResponse chatGptResponseForFeedback = null;
-        chatGptResponseForFeedback = chatGptService.askQuestion(selectedMemberId, startDate);
-        String feedbackContent = chatGptResponseForFeedback.getChoices().get(0).getMessage().getContent();
-        feedbackService.save(selectedMemberId, feedbackContent, startDate);
-
-        // 4. 이번 주차 미션과 피드백 문장 생성 후 분석 테이블에 이번 주차 항목들의 OkToUse True로 변경
-        accountAnalyzeService.changeOkToUseWithTrue(selectedMemberId);
-
-        // 5. profile goalEntry 업데이트
-        profileService.updateGoalEntry(selectedMemberId);
-
-        return surveyDTO;
-    }
-
-    // 설문조사 보여주기(로그인했을 때 id 가져와서)
-
-    @RequestMapping("/surveyDTO")
-    @ResponseBody
-    public SurveyDTO sendSurveyDTO(@RequestBody String memberId){
-        SurveyDTO surveyDTO = surveyService.findBySurveyId(memberId);
-        return surveyDTO;
-    }
-
-
-
-    // ************* survey_limit **************
-
-    private final SurveyLimitService surveyLimitService;
-
-    @RequestMapping("/surveyLimitDTO")
-    @ResponseBody
-    public SurveyLimitDTO sendSurveyLimit(@RequestBody SurveyDTO surveyDTO){
-        int entry1totalAmount = accountAnalyzeService.findbySurveyIdAndEntry(surveyDTO.getSurveyId(), surveyDTO.getGoalEntry1());
-        int entry2totalAmount = accountAnalyzeService.findbySurveyIdAndEntry(surveyDTO.getSurveyId(), surveyDTO.getGoalEntry2());
-        int entry3totalAmount = accountAnalyzeService.findbySurveyIdAndEntry(surveyDTO.getSurveyId(), surveyDTO.getGoalEntry3());
-
-        SurveyLimitDTO surveyLimitDTO = surveyLimitService.save(surveyDTO.getSurveyId(), entry1totalAmount, entry2totalAmount, entry3totalAmount);
-        return surveyLimitDTO;
-    }
 
 }
 //MemberController.class
